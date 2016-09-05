d35c90e83eeac9d8754b6b32af9547c480986027
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-09-05 19:59:46 +1000

    added carousel

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -65,23 +65,4 @@ angular.module('upToApp')
 
       return array;
     }
-
-  //   $scope.slides = [
-  //  {
-  //        title:'7 Ways to stay Fit',
-  //        image:baseURL+'sports/',
-  //        text:'Play a sport for 30 minutes a day!'
-  //  },
-  //  {
-  //        title:'Healthly Food',
-  //        image:baseURL+'food/',
-  //        text:'Food that you should be eating!'
-  //  },
-  //  {
-  //        title:'Relaxing Holidays',
-  //        image:baseURL+'nature/',
-  //        text:'10 Locations for Nature Lovers!'
-  //  }
-   //
-  //   ];
   }]);
